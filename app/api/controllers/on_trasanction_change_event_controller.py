f4bc73d90ddc37099f4b8d450e3a07e2e2aa31f0
Jose De avila <jrdeavila@unicesar.edu.co>
2024-01-18 17:13:49 -0500

    Removing id query parameter

--- app/api/controllers/on_trasanction_change_event_controller.py
+++ app/api/controllers/on_trasanction_change_event_controller.py
@@ -1,13 +1,12 @@
 from typing import Union
+
 from fastapi import APIRouter, Depends
+
+from api.requests.transaction_event_request import TransactionEventRequest
 from core.application.use_cases.update_payment_status_use_case import (
     UpdatePaymentStatusUseCase,
 )
-
 from core.domain.entities.payment import Payment
-from api.requests.transaction_event_request import (
-    TransactionEventRequest,
-)
 from core.infrastructure.injection.inject_use_cases import (
     inject_update_payment_status_use_case,
 )
@@ -19,12 +18,12 @@ class OnTransactionChangeEventCtrl:
     @router.post("/")
     def on_transaction_change_event(
         transaction_event_request: TransactionEventRequest,
-        id: Union[str, None] = None,
         update_payment_status_use_case: UpdatePaymentStatusUseCase = Depends(
             inject_update_payment_status_use_case
         ),
     ) -> Payment:
         payment = update_payment_status_use_case.execute(
-            id, transaction_event_request.data.transaction.status
+            transaction_event_request.data.transaction.id,
+            transaction_event_request.data.transaction.status,
         )
         return payment
