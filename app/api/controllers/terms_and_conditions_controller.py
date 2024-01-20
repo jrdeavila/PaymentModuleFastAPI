9a66da3617f27d72f7fe5fc98ac2d69119689a89
Jose De avila <jrdeavila@unicesar.edu.co>
2024-01-20 13:00:36 -0500

    Change Endpoint name

--- app/api/controllers/terms_and_conditions_controller.py
+++ app/api/controllers/terms_and_conditions_controller.py
@@ -12,7 +12,7 @@ from core.infrastructure.injection.inject_use_cases import (
 class TermsAndConditionController:
     router = APIRouter()
 
-    @router.get("/terms-and-conditions")
+    @router.get("/")
     async def get_terms_and_conditions(
         terms_and_conditions_use_case: GetTermsAndConditionsUseCase = Depends(
             inject_get_terms_and_conditions_use_case
